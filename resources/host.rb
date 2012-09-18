f3a5c9bebc36c01ae436ea836426514a262b0b06
Robert <taeil.choi@kt.com>
2012-09-18 13:21:32 +0900

    remove initialize def in LWRP

--- resources/host.rb
+++ resources/host.rb
@@ -7,8 +7,3 @@ attribute :hostname, :kind_of => String, :name_attribute => true
 attribute :macaddress, :kind_of => String
 attribute :ipaddress, :kind_of => String
 attribute :options, :kind_of => Array, :default => []
-
-def initialize(*args)
-  super
-  @action = :add
-end
