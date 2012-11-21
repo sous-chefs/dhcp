2d8fa9fd80dd153b82fda62468fffceb2e2afd7c
Jesse Nelson <spheromak@gmail.com>
2012-11-21 11:00:16 +0900

    bump version to 2.0.0

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-maintainer       "Opscode, Inc."
-maintainer_email "matt@opscode.com"
+maintainer       "Jesse Nelson"
+maintainer_email "spheromak@gmail.com"
 license          "Apache 2.0"
 description      "DHCP"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "2.0.0"
 
-#tested with Ubuntu, assume Debian works similarly 
-%w{ ubuntu centos redhat }.each do |os|
+#tested with Ubuntu, assume Debian works similarly
+%w{ debian ubuntu centos redhat }.each do |os|
   supports os
 end
 
