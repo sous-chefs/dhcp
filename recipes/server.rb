6f733bdbecbb7fa7f57ce89c257a4dad48efee5e
Jesse Nelson <spheromak@gmail.com>
2012-11-21 13:46:33 +0900

    move config for dhcpd to _config

--- recipes/server.rb
+++ recipes/server.rb
@@ -22,55 +22,12 @@
 
 # load dynadns with current context
 
-include_recipe "git"
 include_recipe "helpers-databags"
-include_recipe "helpers-search::mix_recipe"
 include_recipe "ruby-helper"
 
 include_recipe "dhcp::_package"
 include_recipe "dhcp::_service"
-
-DHCP::Failover.load(node)
-DHCP::DynaDns.load(node)
-
-#
-# Global DHCP config settings
-#
-template node[:dhcp][:config_file] do
-  owner "root"
-  group "root"
-  mode 0644
-  source "dhcpd.conf.erb"
-  variables(
-    :allows => node['dhcp']['allows'] || [], 
-    :parameters =>  node['dhcp']['parameters'] || [],
-    :options =>  node[:dhcp][:options] || [],
-    :ddns => DHCP::DynaDns.rndc_keys,
-    :my_ip => node[:ipaddress],
-    :role => DHCP::Failover.role,
-    :peer_ip => DHCP::Failover.peer,
-    :failover => DHCP::Failover.enabled?
-    )
-  action :create
-  notifies :restart, resources(:service => node[:dhcp][:service_name] ), :delayed
-end
-
-#
-# Create the dirs and stub files for each resource type
-#
-%w{groups.d hosts.d subnets.d}.each do |dir|
-  directory "#{node[:dhcp][:dir]}/#{dir}"
-  file "#{node[:dhcp][:dir]}/#{dir}/list.conf" do
-    action :create_if_missing
-    content ""
-  end
-end
-
-
-include_recipe  "dhcp::_networks"
-include_recipe  "dhcp::_groups"
-include_recipe  "dhcp::_hosts"
-
-
-
-
+include_recipe "dhcp::_config"
+include_recipe "dhcp::_networks"
+include_recipe "dhcp::_groups"
+include_recipe "dhcp::_hosts"
