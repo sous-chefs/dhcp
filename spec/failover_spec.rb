bbd2248f21ca22468bb4d8bcbdf105f64d252230
Jesse Nelson <spheromak@gmail.com>
2012-11-21 20:53:18 +0900

    more tests

--- spec/failover_spec.rb
+++ spec/failover_spec.rb
@@ -9,19 +9,20 @@ def failover_search(result)
 end
 
 describe "DHCP::Failover" do
-  before do 
+  before(:each) do 
     Fauxhai.mock(platform:'ubuntu', version:'12.04')
     @chef_run = ChefSpec::ChefRunner.new
   end
 
   it 'should detect when disabled' do
+    failover_search(Array.new)
     DHCP::Failover.load(@chef_run.converge.node) 
     DHCP::Failover.enabled?.should be_false
   end
 end
 
 describe 'DHCP::Failover Master' do
-  before do
+  before(:each) do
     dummy_nodes
 
     Fauxhai.mock(platform:'ubuntu', version:'12.04') do |node|
@@ -34,7 +35,7 @@ describe 'DHCP::Failover Master' do
     DHCP::Failover.load(@chef_run.converge.node)
   end
 
-  it 'should know its primary' do
+  it 'should identify as primary' do
     DHCP::Failover.role.should == 'primary' 
   end
 
@@ -44,7 +45,7 @@ describe 'DHCP::Failover Master' do
   end
 
   it 'should enable when secondary found' do
-    failover_search([@slave.to_hash, 0, 1])
+    failover_search([@master.to_hash])
     DHCP::Failover.enabled?.should be_true
   end
 
@@ -55,17 +56,37 @@ describe 'DHCP::Failover Master' do
 end
 
 describe "DHCP::Failover Slave" do
-  it 'should identify as slave' do
+  before(:each) do
+    dummy_nodes
+
+    Fauxhai.mock(platform:'ubuntu', version:'12.04') do |node|
+      node[:dhcp] ||= Hash.new
+      node[:dhcp][:slave] = true
+      node[:ipaddress] = "10.1.1.20"
+    end
+
+    @chef_run = ChefSpec::ChefRunner.new
+    DHCP::Failover.load(@chef_run.converge.node)
+  end
+
+  it 'should identify as secondary' do
+    DHCP::Failover.role.should == 'secondary' 
   end
 
   it 'should disable when no primaries found' do
-  #  DHCP::Failover.load(chef_run.converge.node)  
-  #  DHCP::Failover.enabled?.should be_false
+    failover_search(Array.new)
+    DHCP::Failover.enabled?.should be_false
   end
 
-  it 'should enable when primary found' do 
-  #  DHCP::Failover.load(chef_run.converge.node)  
-  #  DHCP::Failover.enabled?.should be_false
+  it 'should enable when primary found' do
+    failover_search([ @master.to_hash, 0, 1 ])
+    DHCP::Failover.enabled?.should be_true
   end
+  
+  it 'should find our peer' do
+    failover_search([ @master.to_hash, 0, 1 ])
+    DHCP::Failover.peer.should == '10.1.1.10'
+  end
+
 end
 
