12dffb86967f55229e12531211d2694d71ca00bb
Jesse Nelson <spheromak@gmail.com>
2012-11-21 12:18:36 +0900

    use new search singleton, remove all the run_context juggling

--- libraries/failover.rb
+++ libraries/failover.rb
@@ -1,35 +1,10 @@
 module DHCP
   module Failover
     class << self
-      include Chef::Mixin::Language
 
-      attr_accessor :run_context
-      attr_reader :slaves, :masters, :role, :peer
-
-      def load(run_context)
-        @run_context =  run_context
-      end
-
-      def node
-        run_context.node
-      end
-
-      def scoped_search(scope, *args, &block)
-        # extract the original query
-        query = args[1] || "*:*"
-
-        # prepend the environment to the query based on scope
-        case scope
-        when /domain/i,/dc/i
-          args[1] = "domain:#{node[:domain]}  AND (#{query})"
-        when /environment/, /env/i
-          args[1] = "chef_environment:#{node[:chef_environment]} AND (#{query})"
-        else
-          rasie ArgumentError  "restricted search does not know how to handle scope: #{scope} "
-        end
-
-        # call the original search method
-        Chef::Search::Query.new.search((*args, &block)
+      attr :node
+      def load(node)
+        @node = node
       end
 
       def enabled?
@@ -54,18 +29,18 @@ module DHCP
       end
 
       def peer
-        slave  =  slaves.sort.first
-        Chef::Log.info "Slave: #{slave} "
+        slave  =  slaves.first
+        Chef::Log.info "Dhcp Slave: #{slave}"
         return nil if slave.blank?
         slave.ipaddress
       end
 
       def slaves
-        scoped_search("domain", :node, "dhcp_slave:true")
+        Helpers::Search.scoped({:domain => node[:domain]}, :node, "dhcp_slave:true")
       end
 
       def masters
-        scoped_search("domain", :node, "dhcp_master:true")
+        Heplers::Search.scoped({:domain => node[:domain]}, :node, "dhcp_master:true")
       end
 
     end
