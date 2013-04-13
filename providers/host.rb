2b786f8f6270ed629b9379095cef581a3ad38ebb
Jesse Nelson <spheromak@gmail.com>
2013-04-13 05:41:52 -0700

    Update for chef11 and fc rule fixes

--- providers/host.rb
+++ providers/host.rb
@@ -1,3 +1,4 @@
+use_inline_resources 
 
 def write_include 
   file_includes = []
@@ -7,22 +8,21 @@ def write_include
     end
   end
 
-  t = template "#{new_resource.conf_dir}/hosts.d/list.conf" do
+  template "#{new_resource.conf_dir}/hosts.d/list.conf" do
     cookbook "dhcp"
     source "list.conf.erb"
     owner "root"
     group "root"
     mode 0644
     variables( :files => file_includes )
-    notifies :restart, resources(:service => node[:dhcp][:service_name]), :delayed
+    notifies :restart, "service[#{node[:dhcp][:service_name]}]", :delayed
   end
-  new_resource.updated_by_last_action(t.updated?)
 end
 
 action :add do
   directory "#{new_resource.conf_dir}/hosts.d/"
 
-  t = template  "#{new_resource.conf_dir}/hosts.d/#{new_resource.hostname}.conf" do
+  template  "#{new_resource.conf_dir}/hosts.d/#{new_resource.hostname}.conf" do
     cookbook "dhcp"
     source "host.conf.erb"
     variables(
@@ -36,18 +36,16 @@ action :add do
     owner "root"
     group "root"
     mode 0644
-    notifies :restart, resources(:service => node[:dhcp][:service_name] ), :delayed
+    notifies :restart, "service[#{node[:dhcp][:service_name]}]", :delayed
   end
-  new_resource.updated_by_last_action(t.updated?)
   write_include
 end
 
 action :remove do
-  f = file "#{new_resource.conf_dir}/hosts.d/#{new_resource.name}.conf" do
+  file "#{new_resource.conf_dir}/hosts.d/#{new_resource.name}.conf" do
     action :delete
-    notifies :restart, resources(:service => node[:dhcp][:service_name]), :delayed
+    notifies :restart, "service[#{node[:dhcp][:service_name]}]", :delayed
   end
-  new_resource.updated_by_last_action(f.updated?)
 
   write_include
 end
