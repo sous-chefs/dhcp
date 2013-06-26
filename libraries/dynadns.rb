635598c1d7d90b6fb058cf17dfbdff8478a5b5a9
Stephan Renatus <s.renatus@cloudbau.de>
2013-06-26 11:57:53 +0200

    Fix bug wrt. Chef 10.x.x
    See also [COOK-2234](https://tickets.opscode.com/browse/COOK-2234).

--- libraries/dynadns.rb
+++ libraries/dynadns.rb
@@ -2,13 +2,15 @@
 module DHCP
   module DynaDns 
     class  << self  
-      if  Chef::Version.new(Chef::VERSION) <= Chef::Version.new( "10.16.2" )
+      if  Chef::Version.new(Chef::VERSION) < Chef::Version.new( "11.0.0" )
         include Chef::Mixin::Language
       else
         include Chef::DSL::DataQuery
       end
 
-      attr :node, :zones, :keys
+      attr :node
+      attr :zones
+      attr :keys
 
       def load(node)
         @node = node
