f33ff3d0fb5454b170859fd0be513c13e97c8aed
Wil Reichert <wil.reichert@kt.com>
2013-11-04 15:40:04 +0900

    setup correct per distro iface variable
    Conflicts:
    	.kitchen.yml

--- recipes/_service.rb
+++ recipes/_service.rb
@@ -18,6 +18,6 @@ template node[:dhcp][:init_config] do
   source "init_config.erb"
   variables(
     :interfaces => node[:dhcp][:interfaces],
-    :var  =>  "DHCPDARGS"
+    :var => node[:dhcp][:init_iface]
   )
 end
