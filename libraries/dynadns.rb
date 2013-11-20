25126b433c5e38ddb65851dde847357d22625a13
Jesse Nelson <spheromak@gmail.com>
2013-11-20 13:39:20 -0800

    zone check should short honor node[:dhcp][:use_bags]

--- libraries/dynadns.rb
+++ libraries/dynadns.rb
@@ -1,14 +1,15 @@
 
 module DHCP
-  module DynaDns 
-    class  << self  
+  module DynaDns
+    class  << self
       if  Chef::Version.new(Chef::VERSION) <= Chef::Version.new( "10.16.2" )
         include Chef::Mixin::Language
       else
         include Chef::DSL::DataQuery
       end
 
-      attr :node, :zones, :keys
+      attr_accessor :node
+      attr_accessor :zones, :keys
 
       def load(node)
         @node = node
@@ -27,25 +28,25 @@ module DHCP
           masters[name] ||= Hash.new
 
           # set to global master by default
-          if node[:dns].has_key? :master and node[:dns][:master].blank? != true
-            masters[name]["master"] = node[:dns][:master] 
+          if node[:dns].has_key? :master and node[:dns][:master].blank? == false
+            masters[name]["master"] = node[:dns][:master]
           end
 
-          if node[:dns].has_key? :rndc_key and node[:dns][:rndc_key].blank? != true
+          if node[:dns].has_key? :rndc_key and node[:dns][:rndc_key].blank? == false
             masters[name]["key"] = node[:dns][:rndc_key]
           end
 
           # use zone bag override if it exists
-          if zone.has_key? "master_address" and  zone["master_address"].blank? != true
+          if zone.has_key? "master_address" and  zone["master_address"].blank? == false
             masters[name]["master"] = zone["master_address"]
           end
 
-          if zone.has_key? "rndc_key" and zone["rndc_key"].blank? != true
+          if zone.has_key? "rndc_key" and zone["rndc_key"].blank? == false
              masters[name]["key"] = zone["rndc_key"]
           end
 
           # validate
-          unless masters[name].has_key? "key" and masters[name].has_key? "master" 
+          unless masters[name].has_key? "key" and masters[name].has_key? "master"
             masters.delete(name)
           end
         end
@@ -60,7 +61,7 @@ module DHCP
       def keys
         k ||= Hash.new
         @zones ||= load_zones
-        return unless @zones 
+        return if @zones.blank?
 
         # global default keys if they exist
         if node.has_key? :dns  and node[:dns].has_key? :rndc_key
@@ -69,24 +70,24 @@ module DHCP
 
         @zones.each do |zone|
           name = zone["zone_name"]
-          if zone.has_key? "rndc_key" 
+          if zone.has_key? "rndc_key"
             k[zone['rndc_key']] = data_bag_item("rndc_keys", zone["rndc_key"]).to_hash
           end
         end
         k
       end
 
-      # 
+      #
       # Load all zone bags this node calls out
-      # 
+      #
       def load_zones
-        unless node.has_key? :dns and node[:dns].has_key? :zones and node[:dns][:zones].blank? != true 
+        unless node[:dhcp][:use_bags] == true and node.has_key? :dns and node[:dns].has_key? :zones and node[:dns][:zones].blank? != true
           return nil
         end
 
         @zones =  Array.new
         node[:dns][:zones].each do |zone|
-          bag_name = node[:dns][:bag_name] || "dns_zones" 
+          bag_name = node[:dns][:bag_name] || "dns_zones"
           zones << data_bag_item(bag_name, Helpers::DataBags.escape_bagname(zone) ).to_hash
         end
         @zones
