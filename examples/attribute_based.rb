ae61d349c9403f87b2465f904d3e1733f1a939cf
Jesse Nelson <spheromak@gmail.com>
2014-07-10 18:18:25 -0700

    add attribute example

---
+++ examples/attribute_based.rb
@@ -0,0 +1,40 @@
+# Example Attribute driven  wrapper recipe
+
+# rubocop:disable LineLength
+node.default[:dhcp][:options]['domain-name'] = '"dev.f00bar.com"'
+node.default[:dhcp][:options]['domain-name-servers'] = '10.33.87.98, 10.33.87.99'
+node.default[:dhcp][:options]['domain-search'] = '"dev.f00bar.com","f00bar.com"'
+# rubocop:enable All
+
+include_recipe 'dhcp::_package'
+include_recipe 'dhcp::_service'
+include_recipe 'dhcp::_config'
+
+rndc_key = {
+  'name' => 'dev.f00bar.com',
+  'algorithm' => 'hmac-md5',
+  'secret' => 'CHANGETHISYOURSELF'
+}
+
+dns_zones = [
+  {
+    'zone' => 'dev.f00bar.com',
+    'primary' => 'ns01.dev.f00bar.com',
+    'key' => 'dev.f00bar.com'
+  },
+  {
+    'zone' => '0.87.33.10.in-addr.arpa.',
+    'primary' => 'ns01.dev.f00bar.com',
+    'key' => 'dev.f00bar.com'
+  }
+]
+
+dhcp_subnet '10.33.87.0' do
+  conf_dir node[:dhcp][:dir]
+  netmask '255.255.255.0'
+  broadcast '10.33.87.255'
+  routers ['10.33.87.1']
+  range '10.33.87.5 10.33.87.254'
+  key rndc_key
+  zones dns_zones
+end
