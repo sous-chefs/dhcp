d459332396af00c758dca00b8f09911fc9737fac
Jacob McCann <jacob.mccann2@target.com>
2015-07-21 19:24:58 -0500

    Fix serverspec testing

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 # simple example
-describe package( "yum" ) do
-  case backend(Serverspec::Commands::Base).check_os[:family]
-  when "RedHat"
+describe package('yum') do
+  case os[:family]
+  when 'redhat'
     it { should be_installed }
   else
     it { should_not be_installed }
