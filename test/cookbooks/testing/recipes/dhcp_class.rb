8c86e8ac61b450741885116b0c20f8d13eed1e8b
Jacob McCann <jacob.mccann2@target.com>
2015-09-10 18:34:11 -0500

    Load classes before subnets

--- test/cookbooks/testing/recipes/dhcp_class.rb
+++ test/cookbooks/testing/recipes/dhcp_class.rb
@@ -7,3 +7,14 @@ dhcp_class 'RegisteredHosts' do
   subclass '1:10:bf:48:42:55:01'
   subclass '1:10:bf:48:42:55:02'
 end
+
+dhcp_subnet 'deny host from class' do
+  subnet '192.168.4.0'
+  broadcast '192.168.4.255'
+  netmask '255.255.255.0'
+  routers ['192.168.4.1']
+  pool do
+    range '192.168.4.20 192.168.4.30'
+    deny 'members of "RegisteredHosts"'
+  end
+end
