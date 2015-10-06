995bdba40b41e4f7e674270957490e6cc0727430
Brett Petrusek <pretusek@us.ibm.com>
2015-10-06 14:43:13 -0400

    added variable passing for allow to template

--- test/cookbooks/testing/recipes/dhcp_class.rb
+++ test/cookbooks/testing/recipes/dhcp_class.rb
@@ -18,3 +18,14 @@ dhcp_subnet 'deny host from class' do
     deny 'members of "RegisteredHosts"'
   end
 end
+
+dhcp_subnet 'allow host from class' do
+  subnet '192.168.4.0'
+  broadcast '192.168.4.255'
+  netmask '255.255.255.0'
+  routers ['192.168.4.1']
+  pool do
+    range '192.168.4.20 192.168.4.30'
+    deny 'allow of "RegisteredHosts"'
+  end
+end
