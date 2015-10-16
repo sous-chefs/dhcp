8563858cf1fc88c985761182d09c11588c06304a
Jacob McCann <jacob.mccann2@target.com>
2015-10-16 15:08:28 -0700

    More conversions to use cached chef_run

--- spec/providers/dhcp_shared_network_spec.rb
+++ spec/providers/dhcp_shared_network_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'testing::dhcp_shared_network' do
-  let(:chef_run) do
+  cached(:chef_run) do
     ChefSpec::ServerRunner.new(platform: 'centos', version: '6.6', step_into: ['dhcp_shared_network']).converge(described_recipe)
   end
 
