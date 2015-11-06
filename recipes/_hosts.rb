7b211459202a62e82f7ca73eaa7063b9f234d821
Jordan Evans <jordane@osuosl.org>
2015-11-06 13:14:28 -0800

    After commit 35d01b8e4e66e7aa94e102ce873bbf067cd61b89, fully remove Helpers::DataBag

--- recipes/_hosts.rb
+++ recipes/_hosts.rb
@@ -28,7 +28,7 @@ unless node['dhcp']['hosts'].empty?
 
   host_list.each do  |host|
     if node['dhcp']['use_bags'] == true
-      host_data = data_bag_item(node['dhcp']['hosts_bag'], Helpers::DataBags.escape_bagname(host))
+      host_data = data_bag_item(node['dhcp']['hosts_bag'], Dhcp::Helpers.escape_bagname(host))
     else
       host_data = node['dhcp']['host_data'].fetch host, nil
     end
