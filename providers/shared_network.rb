966b065a433a065034fede34b0a6bb58fc6972dc
Grant Ridder <shortdudey123@gmail.com>
2017-01-03 16:35:40 -0800

    Fix copyright year

--- providers/shared_network.rb
+++ providers/shared_network.rb
@@ -3,7 +3,7 @@
 # Cookbook Name:: dhcp
 # Provider:: dhcp_shared_subnet
 #
-# Copyright:: 2016, Sous Chefs
+# Copyright:: 2015-2016, Sous Chefs
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
