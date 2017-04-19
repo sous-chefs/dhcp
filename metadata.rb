641181d6fd3ac2b077f80e01d1c95984da99cb74
Tim Smith <tsmith@chef.io>
2017-04-18 23:16:00 -0700

    Remove Chef 11 compatibility
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,8 @@ description       'Installs and configures DHCP'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '5.4.0'
 
-source_url "https://github.com/sous-chefs/#{name}" if respond_to?(:source_url)
-issues_url "https://github.com/sous-chefs/#{name}/issues" if respond_to?(:issues_url)
+source_url "https://github.com/sous-chefs/#{name}"
+issues_url "https://github.com/sous-chefs/#{name}/issues"
 
 # tested with Ubuntu, assume Debian works similarly
 %w(debian ubuntu centos redhat scientific oracle).each do |os|
