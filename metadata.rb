07a3e977b5215379257131bc22b26503e63b1e75
Tim Smith <tsmith@chef.io>
2017-04-18 23:15:15 -0700

    Use a standard license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'dhcp'
 maintainer        'Sous Chefs'
 maintainer_email  'help@sous-chefs.org'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Installs and configures DHCP'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '5.4.0'
