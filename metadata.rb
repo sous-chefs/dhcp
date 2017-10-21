e8cc6232db50b3b84d3335d03a3a5db07ffb1fb0
Dan Webb <dan.webb@damacus.io>
2017-10-21 22:21:52 +0100

    Remove the attribute that was failing in Chef 13. This is likely to break search. Use major platform numbers so we can track minors easier. Add foodcritic file

--- metadata.rb
+++ metadata.rb
@@ -4,14 +4,11 @@ maintainer_email  'help@sous-chefs.org'
 license           'Apache-2.0'
 description       'Installs and configures DHCP'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+chef_version      '>= 12.1' if respond_to?(:chef_version)
+source_url        'https://github.com/sous-chefs/dhcp'
+issues_url        'https://github.com/sous-chefs/dhcp/issues'
 version           '5.4.0'
 
-source_url "https://github.com/sous-chefs/#{name}"
-issues_url "https://github.com/sous-chefs/#{name}/issues"
-
-# tested with Ubuntu, assume Debian works similarly
 %w(debian ubuntu centos redhat scientific oracle).each do |os|
   supports os
 end
-
-chef_version '>= 12.1' if respond_to?(:chef_version)
