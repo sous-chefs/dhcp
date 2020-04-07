0ed1a705f4215e5518b44a8a20acb983c991315e
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2020-04-07 16:27:40 +0100

    Remove old spec tests

--- spec/server_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'dhcp::server' do
-  let(:chef_run) do
-    ChefSpec::ServerRunner.new do |node|
-      node.default['chef_environment'] = 'production'
-    end.converge(described_recipe)
-  end
-end
