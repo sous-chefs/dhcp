6270935164c5092671f482c92fb5d28efe9b709d
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2021-05-01 12:14:07 +0100

    Template whitespace fixup

--- libraries/template.rb
+++ libraries/template.rb
@@ -1,8 +1,8 @@
 module Dhcp
   module Cookbook
     module TemplateHelpers
-      def nil_or_empty?(property)
-        property.nil? || (property.respond_to?(:empty?) && property.empty?)
+      def nil_or_empty?(*values)
+        values.any? { |v| v.nil? || (v.respond_to?(:empty?) && v.empty?) }
       end
 
       def property_array(property)
@@ -16,7 +16,7 @@ module Dhcp
       end
 
       def property_collection_sorted(property)
-        property.sort.map { |p| p.is_a?(Array) ? p : p.split(' ', 2) }
+        property_collection(property.sort)
       end
     end
   end
